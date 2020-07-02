74f3c574d21f199f081c3ebd491fb680b6fc154a
John Blanchard <john@theblanchard.com>
2020-07-02 13:02:55 -0700

    First commit.
    First commit for AFTRSHCK gaming.

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>AFTRSHCK GAMING</title>
+
+    <meta name="description" content="Streaming site for AFTRSHCK Gaming">
+    <meta name="author" content="John Blanchard">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					 
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="navbar-toggler-icon"></span>
+					</button> <a class="navbar-brand" href="#">AFTRSHCK</a>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="navbar-nav ml-md-auto">
+							<li class="nav-item active">
+								 <a class="nav-link" href="https://www.youtube.com/channel/UCOVtRL572XzLLxKrJJxcK6A">Jacob Galland Gaming</span></a>
+							</li>
+	<!--
+							<li class="nav-item dropdown">
+								 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+								<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+									 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+									<div class="dropdown-divider">
+									</div> <a class="dropdown-item" href="#">Separated link</a>
+								</div>
+							</li>
+	-->
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">	
+			<img src="img/logo.jpg">
+		</div>
+	</div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
